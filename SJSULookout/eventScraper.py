5be5485c57cf3f87b81b2451e10e0efd1635760f
iamtheyam <austin_yam@yahoo.com>
2018-06-01 11:41:06 -0700

    json conversion

--- SJSULookout/eventScraper.py
+++ SJSULookout/eventScraper.py
@@ -5,6 +5,8 @@ from datetime import datetime
 from datetime import timedelta
 from bs4 import BeautifulSoup
 
+print('starting script')
+
 now = datetime.now()
 strnow = str(now)
 strnow = strnow.split(' ')[0].split('-')
@@ -15,6 +17,7 @@ strEnd = str(end)
 strEnd = strEnd.split(' ')[0].split('-')
 ey, em, ed = strEnd
 
+print('getting quote page')
 quote_page = 'https://events.sjsu.edu/EventList.aspx?fromdate='+mm+'%2f'+dd+'%2f'+yy+'&todate='+em+'%2f'+ed+'%2f'+ey
 
 
@@ -24,16 +27,29 @@ soup = BeautifulSoup(page, 'html.parser')
 table = soup.find('table', attrs= {'id':'tblEvents'})
 
 
-
 #for dates in table.find_all('h4', attrs={'class':'adx_wcag_header'}):
 	#date = dates.text
 	#print(date)
-	
+
+database = {
+	'events': {},
+}
+
+event_id = 0
+
+print('populating database')
+
 #Getting the links for each event
 for titles in table.find_all('div', attrs={'class':'vevent'}):
+	event = {}
+
+	event_id += 1
+	event['id'] = event_id
+
 	title = titles.find('a', attrs={'class':'url summary'})['href']
-	#print(title)
+	
 	new_page = 'https://events.sjsu.edu/'+str(title)
+	
 	
 	pg = urllib.request.urlopen(new_page)
 	new_soup = BeautifulSoup(pg, 'html.parser')
@@ -42,7 +58,8 @@ for titles in table.find_all('div', attrs={'class':'vevent'}):
 
 	#eventName
 	name = data.find('td', attrs={'class':'listheadtext'}).text
-	#print(name)
+	event['title'] = name
+	print(name)
 	
 	desc = data.find_all('td',attrs={'class':'detailsview'})
 
@@ -57,6 +74,8 @@ for titles in table.find_all('div', attrs={'class':'vevent'}):
 		event_text = event_text.strip()
 	except Exception as e:
 		event_text = None
+
+	event['description'] = event_text
 	
 
 	try:
@@ -65,13 +84,17 @@ for titles in table.find_all('div', attrs={'class':'vevent'}):
 		date_text = date_text.strip()
 	except Exception as e:
 		date_text = None
+
+	event['date'] = date_text
 	
 	try:
 	#eventStartTime
 		start_time = text[text.index("Start Time:") + len("Start Time:"): text.index("End Date:")]
 		start_time = start_time.strip()
 	except Exception as e:
-		start_time = None	
+		start_time = None
+
+	event['start_time'] = start_time
 	
 	try:
 	#eventEndTime
@@ -79,6 +102,8 @@ for titles in table.find_all('div', attrs={'class':'vevent'}):
 		end_time = end_time.strip()
 	except Exception as e:
 		end_time = None
+
+	event['end_time'] = end_time
 	
 	try:
 	#eventLocation
@@ -86,6 +111,8 @@ for titles in table.find_all('div', attrs={'class':'vevent'}):
 		event_location = event_location.strip()
 	except Exception as e:
 		event_location = None
+
+	event['location'] = event_location
 	
 	try:
 	#eventContact
@@ -93,6 +120,8 @@ for titles in table.find_all('div', attrs={'class':'vevent'}):
 		event_contact = event_contact.strip()
 	except Exception as e:
 		event_contact = None
+
+	event['contact'] = event_contact
 		
 	try:
 	#eventType
@@ -100,17 +129,13 @@ for titles in table.find_all('div', attrs={'class':'vevent'}):
 		event_type = event_type.strip()
 	except Exception as e:
 		event_type = None
-		
+
+	event['type'] = event_type
+	
+	database['events'][event_id] = event
+
 	#print(event_type)
 
-		
-
-			
-	
-	
-	
-	
-		
-	
-	
-	
+import json
+with open('events.json', 'w') as outfile:
+    json.dump(database, outfile)
